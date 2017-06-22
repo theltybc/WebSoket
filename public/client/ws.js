babb5f64752d21fe6879f0238030a941b24def0b
we <1>
2017-06-22 20:11:44 +0500

    REPLACE цвета

--- public/client/ws.js
+++ public/client/ws.js
@@ -100,7 +100,7 @@ function showMsg(cl, msg, error, errorParse) {
     if (error) {
         el.css('background-color', 'darkred').css('color', '#f3fdff');
     } else if (errorParse) {
-        el.css('color', '#000d30')
+        el.css('color', '#00154e')
     }
     $message_field.prepend(el);
 }
